5a6139f3ae728df19a5092c02c7db32061a235e2
Mark Conroy <info@adesignforlife.net>
2016-06-06 12:39:51 +0100

    call the statuses from a json file

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
   if (isset($_POST['submit'])) {
     $status = $_POST['status'];
-    echo $status;
+    echo htmlspecialchars($status);
   }
+?>
+
+<?php
+$statuses_file = file_get_contents('statuses.json');
+$statuses_array = json_decode($statuses_file);
+$statuses = $statuses_array;
 ?>
 
 <!DOCTYPE html>
@@ -16,5 +22,15 @@
     <input name="status"></input>
     <input name="submit" type="submit" />
   </form>
+
+  <?php
+  if (is_array($statuses)) {
+    foreach ($statuses as $status) {
+      print '<p>This is status id: ' . $status->fmrlid . '<br>
+      it says: ' . $status->fmrlstatus . '</p>';
+    }
+  }
+  ?>
+  
 </body>
 </html>
